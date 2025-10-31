05a769478aad8e2b4b1ba46f970a4a34540698b7
IvanEfimov <IvanEfimov@gmail.com>
2025-10-31 14:24:58 +0300

    Various improvements.

---
+++ Sources/Engine/NeoAxis.Core/Utility/ConcurrentLockManager.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (C) NeoAxis Group Ltd. 8 Copthall, Roseau Valley, 00152 Commonwealth of Dominica.
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace NeoAxis
+{
+	public class ConcurrentLockManager<T>
+	{
+		readonly ConcurrentDictionary<T, object> locks = new ConcurrentDictionary<T, object>();
+
+		/////////////////////
+
+		class Releaser : IDisposable
+		{
+			readonly object lockObject;
+
+			public Releaser( object lockObject )
+			{
+				this.lockObject = lockObject;
+			}
+
+			public void Dispose()
+			{
+				Monitor.Exit( lockObject );
+			}
+		}
+
+		/////////////////////
+
+		public IDisposable LockDisposable( T key )
+		{
+			var lockObject = locks.GetOrAdd( key, _ => new object() );
+			Monitor.Enter( lockObject );
+			return new Releaser( lockObject );
+		}
+	}
+}
